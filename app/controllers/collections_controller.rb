4b289aa22e4442d968e250428a2051e6aae968fc
Uthaeus <romanlavery@gmail.com>
2018-04-16 10:22:20 -0600

    Integrated new and create actions for collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -4,4 +4,20 @@ class CollectionsController < ApplicationController
     @collection_items = Collection.all 
   end
 
+  def new
+    @collection_item = Collection.new
+  end
+
+  def create
+    @collection_item = Collection.new(params.require(:collection).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @collection_item.save
+        format.html { redirect_to collections_path, notice: 'Collection Item was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
